66cd963c37573b3d86154e30afa35cc86363fdef
ishchukserhii <ischuksergei@gmail.com>
2025-02-25 00:04:02 +0200

    y

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,13 +6,15 @@ const endPoint = "/api/";
 
 
 
-export function getImg (searchName) {
+export function getImg (searchName, page, perPage) {
     const params = new URLSearchParams({
         key: '48878301-11eef390b295f332c6628756b',
         q: searchName,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
+        per_page: perPage,
+        page: page,
     });
     const url = baseUrl + endPoint + `?${params}`;
     return axios.get(url)
--- src/main.js
+++ src/main.js
@@ -1,92 +1,126 @@
-import {getImg} from './js/pixabay-api.js';
-import {createElemets} from './js/render-functions.js'
+import { getImg } from './js/pixabay-api.js';
+import { createElemets } from './js/render-functions.js';
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
 });
 
 const refs = {
-    btn : document.querySelector('.btn-search'),
-    form : document.querySelector('.search-form'),
-    gallery : document.querySelector('.gallery'),
-    loader: document.querySelector('.msg'),
+  btn: document.querySelector('.btn-search'),
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.msg'),
+  btnLoadMore: document.querySelector('.btn-load'),
+};
+
+const params = {
+  searchText: null,
+  page: null,
+  total: null,
+  perPage: 40,
+};
+
+refs.form.addEventListener('submit', async e => {
+  e.preventDefault();
+  hideLoadMoreBtn();
+  params.page = 1;
+  params.searchText = e.target.elements.bookSearch.value.trim();
+  refs.gallery.innerHTML = '';
+  if (params.searchText === '') {
+    iziToast.show({
+      backgroundColor: 'rgba(255, 67, 67, 0.68)',
+      messageColor: `rgba(255, 255, 255, 1)`,
+      close: `true`,
+      position: 'topRight',
+      title: '🚫',
+      titleColor: '#fff',
+      titleSize: '16px',
+      message: 'Поле пошуку порожнє',
+    });
+    return;
+  } else {
+    refs.loader.innerHTML = '<span class="loader"></span>';
+    try {
+      const res = await getImg(params.searchText, params.page, params.perPage);
+      if (res.data.hits.length === 0) {
+        iziToast.show({
+          backgroundColor: 'rgba(255, 67, 67, 0.68)',
+          messageColor: `rgba(255, 255, 255, 1)`,
+          close: `true`,
+          position: 'topRight',
+          title: '🚫',
+          titleColor: '#fff',
+          titleSize: '16px',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        refs.loader.innerHTML = '';
+      } else {
+        const pushGallery = createElemets(res.data.hits);
+        refs.gallery.innerHTML = pushGallery;
+        lightbox.refresh();
+        refs.loader.innerHTML = '';
+        refs.form.reset();
+        params.total = res.data.total;
+        checkBtnStatus();
+      }
+    } catch (error) {
+      refs.loader.innerHTML = '';
+      iziToast.show({
+        backgroundColor: 'rgba(255, 67, 67, 0.68)',
+        close: `true`,
+        position: 'topRight',
+        title: '🚫',
+        titleColor: '#fff',
+        titleSize: '16px',
+        messageColor: `rgba(255, 255, 255, 1)`,
+        message: 'Охохо.....щось пішло не так....',
+      });
+      console.log(error);
+    }
+  }
+});
+
+refs.btnLoadMore.addEventListener('click', async () => {
+showSpinner();
+  params.page += 1;
+  checkBtnStatus();
+  const res = await getImg(params.searchText, params.page, params.perPage);
+  const markup = createElemets(res.data.hits);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+});
+
+function checkBtnStatus() {
+  const perPage = params.perPage;
+  const maxPage = Math.ceil(params.total / perPage);
+
+  if (params.page >= maxPage) {
+    hideLoadMoreBtn();
+    iziToast.info('This is last page');
+  } else {
+    showLoadMoreBtn();
+  }
+}
+function showLoadMoreBtn() {
+  refs.btnLoadMore.hidden = false;
 }
 
+function hideLoadMoreBtn() {
+  refs.btnLoadMore.hidden = true;
+}
 
+function showSpinner() {
+  refs.loader.classList.remove('hidden');
+}
 
-refs.form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let searchText = e.target.elements.bookSearch.value.trim();
-    refs.gallery.innerHTML = '';
-    if(searchText === ''){
-        iziToast.show ({
-            backgroundColor: 'rgba(255, 67, 67, 0.68)',
-            messageColor: `rgba(255, 255, 255, 1)`,
-            close: `true`,
-            position: "topRight",
-            title: '🚫',
-            titleColor: '#fff',
-            titleSize: '16px',
-            message: 'Поле пошуку порожнє'
-        });
-        return}
-        else{
-            refs.loader.innerHTML = '<span class="loader"></span>';
-            getImg (searchText)
-
-            .then(res => {
-                if (res.data.hits.length === 0){
-                    iziToast.show(
-                        { backgroundColor: 'rgba(255, 67, 67, 0.68)',
-                            messageColor: `rgba(255, 255, 255, 1)`,
-                            close: `true`,
-                            position: "topRight",
-                            title: '🚫',
-                            titleColor: '#fff',
-                            titleSize: '16px',
-                            message: 'Sorry, there are no images matching your search query. Please try again!'
-                        }
-                    )
-                    refs.loader.innerHTML = ''
-                }
-                else{
-                    const pushGallery = createElemets(res.data.hits);
-                    refs.gallery.innerHTML = pushGallery;
-                    lightbox.refresh();
-                    refs.loader.innerHTML = ''
-                    refs.form.reset()
-                }
-            })
-            .catch(error => {
-                refs.loader.innerHTML = '';
-                iziToast.show(
-                    { backgroundColor: 'rgba(255, 67, 67, 0.68)',
-                        messageColor: `rgba(255, 255, 255, 1)`,
-                        close: `true`,
-                        position: "topRight",
-                        title: '🚫',
-                        titleColor: '#fff',
-                        titleSize: '16px',
-                        message: 'Охохо.....щось пішло не так....'
-                    })
-                console.log(error);
-            })
-        }
-    })
-
-
-
-
-
-
-
-
+function hideSpinner() {
+  refs.loader.classList.add('hidden');
+}
